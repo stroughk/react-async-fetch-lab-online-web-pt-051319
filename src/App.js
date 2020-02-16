c1bb5d0ac986dffe371f4ddd9af45c78c8305145
Kateryna Strough <katerynastrough@hotmail.com>
2020-02-16 12:48:50 +0000

    Automatically backed up by Learn

--- src/App.js
+++ src/App.js
@@ -1,1 +1,19 @@
 // create your App component here
+class App extends Component {
+  constructor() {
+    super()
+    this.state {
+      people: {}
+    }
+
+    component DidMount) {
+      fetch ("http://api.open-notify.org/astros.json")
+      .then (response => respons.json())
+      .then(data => {
+        this.setState ({
+          people: data        
+        })
+      })
+    }
+  }
+}
